f8fbd8bfc951dd8ab501ba8c08defa8d1d0001ce
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2022-06-06 18:42:00 -0700

    Update ASLClassifier.cs

--- Fastai-Unity-Tutorial/Assets/Scripts/ASLClassifier.cs
+++ Fastai-Unity-Tutorial/Assets/Scripts/ASLClassifier.cs
@@ -194,6 +194,43 @@ public class ASLClassifier : MonoBehaviour
     }
 
 
+    /// <summary>
+    /// Resize and position the main camera based on an in-scene screen object
+    /// </summary>
+    /// <param name="screenDims">The dimensions of an in-scene screen object</param>
+    private void InitializeCamera(Vector2Int screenDims, string cameraName = "Main Camera")
+    {
+        // Get a reference to the Main Camera GameObject
+        GameObject camera = GameObject.Find(cameraName);
+        // Adjust the camera position to account for updates to the screenDims
+        camera.transform.position = new Vector3(screenDims.x / 2, screenDims.y / 2, -10f);
+        // Render objects with no perspective (i.e. 2D)
+        camera.GetComponent<Camera>().orthographic = true;
+        // Adjust the camera size to account for updates to the screenDims
+        camera.GetComponent<Camera>().orthographicSize = screenDims.y / 2;
+    }
+
+
+    /// <summary>
+    /// Initialize an interface to execute the specified model using the specified backend
+    /// </summary>
+    /// <param name="model">The target model representation</param>
+    /// <param name="workerType">The target compute backend</param>
+    /// <param name="useNCHW">EXPERIMENTAL: The channel order for the compute backend</param>
+    /// <returns></returns>
+    private IWorker InitializeWorker(Model model, WorkerFactory.Type workerType, bool useNCHW = true)
+    {
+        // Validate the selected worker type
+        workerType = WorkerFactory.ValidateType(workerType);
+
+        // Set the channel order of the compute backend to channel-first
+        if (useNCHW) ComputeInfo.channelsOrder = ComputeInfo.ChannelsOrder.NCHW;
+
+        // Create a worker to execute the model using the selected backend
+        return WorkerFactory.CreateWorker(workerType, model);
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -212,7 +249,7 @@ public class ASLClassifier : MonoBehaviour
         // Resize and position the screen object using the source image dimensions
         InitializeScreen();
         // Resize and position the main camera using the source image dimensions
-        Utils.InitializeCamera(screenDims);
+        InitializeCamera(screenDims);
 
         // Get an object oriented representation of the model
         m_RunTimeModel = ModelLoader.Load(modelAsset);
@@ -227,7 +264,7 @@ public class ASLClassifier : MonoBehaviour
         // Add a new Argmax layer
         modelBuilder.Reduce(Layer.Type.ArgMax, argmaxLayer, softmaxLayer);
         // Initialize the interface for executing the model
-        engine = Utils.InitializeWorker(modelBuilder.model, workerType, useNCHW);
+        engine = InitializeWorker(modelBuilder.model, workerType, useNCHW);
 
         // Initialize the GPU output texture
         outputTextureGPU = RenderTexture.GetTemporary(1, 1, 24, RenderTextureFormat.ARGBHalf);
@@ -239,6 +276,72 @@ public class ASLClassifier : MonoBehaviour
 
         // Initialize the webcam dropdown list
         InitializeDropdown();
+    }
+
+
+    /// <summary>
+    /// Process the provided image using the specified function on the GPU
+    /// </summary>
+    /// <param name="image">The target image RenderTexture</param>
+    /// <param name="computeShader">The target ComputerShader</param>
+    /// <param name="functionName">The target ComputeShader function</param>
+    /// <returns></returns>
+    private void ProcessImageGPU(RenderTexture image, ComputeShader computeShader, string functionName)
+    {
+        // Specify the number of threads on the GPU
+        int numthreads = 8;
+        // Get the index for the specified function in the ComputeShader
+        int kernelHandle = computeShader.FindKernel(functionName);
+        // Define a temporary HDR RenderTexture
+        RenderTexture result = RenderTexture.GetTemporary(image.width, image.height, 24, RenderTextureFormat.ARGBHalf);
+        // Enable random write access
+        result.enableRandomWrite = true;
+        // Create the HDR RenderTexture
+        result.Create();
+
+        // Set the value for the Result variable in the ComputeShader
+        computeShader.SetTexture(kernelHandle, "Result", result);
+        // Set the value for the InputImage variable in the ComputeShader
+        computeShader.SetTexture(kernelHandle, "InputImage", image);
+
+        // Execute the ComputeShader
+        computeShader.Dispatch(kernelHandle, result.width / numthreads, result.height / numthreads, 1);
+
+        // Copy the result into the source RenderTexture
+        Graphics.Blit(result, image);
+
+        // Release the temporary RenderTexture
+        RenderTexture.ReleaseTemporary(result);
+    }
+
+
+    /// <summary>
+    /// Scale the source image resolution to the target input dimensions
+    /// while maintaing the source aspect ratio.
+    /// </summary>
+    /// <param name="imageDims"></param>
+    /// <param name="targetDims"></param>
+    /// <returns></returns>
+    private Vector2Int CalculateInputDims(Vector2Int imageDims, int targetDim)
+    {
+        // Clamp the minimum dimension value to 64px
+        targetDim = Mathf.Max(targetDim, 64);
+
+        Vector2Int inputDims = new Vector2Int();
+
+        // Calculate the input dimensions using the target minimum dimension
+        if (imageDims.x >= imageDims.y)
+        {
+            inputDims[0] = (int)(imageDims.x / ((float)imageDims.y / (float)targetDim));
+            inputDims[1] = targetDim;
+        }
+        else
+        {
+            inputDims[0] = targetDim;
+            inputDims[1] = (int)(imageDims.y / ((float)imageDims.x / (float)targetDim));
+        }
+
+        return inputDims;
     }
 
 
@@ -326,7 +429,7 @@ public class ASLClassifier : MonoBehaviour
         // Resize and position the screen object using the source image dimensions
         InitializeScreen();
         // Resize and position the main camera using the source image dimensions
-        Utils.InitializeCamera(screenDims);
+        InitializeCamera(screenDims);
     }
 
 
@@ -348,7 +451,7 @@ public class ASLClassifier : MonoBehaviour
                 // Resize and position the screen object using the source image dimensions
                 InitializeScreen();
                 // Resize and position the main camera using the source image dimensions
-                Utils.InitializeCamera(screenDims);
+                InitializeCamera(screenDims);
             }
         }
         else if (webcamTexture && webcamTexture.isPlaying)
@@ -359,11 +462,11 @@ public class ASLClassifier : MonoBehaviour
             // Resize and position the screen object using the source image dimensions
             InitializeScreen();
             // Resize and position the main camera using the source image dimensions
-            Utils.InitializeCamera(screenDims);
+            InitializeCamera(screenDims);
         }
 
         // Scale the source image resolution
-        Vector2Int inputDims = Utils.CalculateInputDims(screenDims, targetDim);
+        Vector2Int inputDims = CalculateInputDims(screenDims, targetDim);
         if (printDebugMessages) Debug.Log($"Input Dims: {inputDims.x} x {inputDims.y}");
 
         // Initialize the input texture with the calculated input dimensions
@@ -376,7 +479,7 @@ public class ASLClassifier : MonoBehaviour
         if (SystemInfo.supportsComputeShaders)
         {
             // Normalize the input pixel data
-            Utils.ProcessImageGPU(inputTexture, processingShader, "NormalizeImageNet");
+            ProcessImageGPU(inputTexture, processingShader, "NormalizeImageNet");
 
             // Initialize a Tensor using the inputTexture
             input = new Tensor(inputTexture, channels: 3);
